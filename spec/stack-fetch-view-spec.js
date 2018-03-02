ee9c04b1325d45c47d7ac19ef056f486c95ae0cc
izzydoesit <israeldmatos@gmail.com>
2018-03-02 09:33:58 -0800

    Initial commit - package generated

---
+++ spec/stack-fetch-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import StackFetchView from '../lib/stack-fetch-view';
+
+describe('StackFetchView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
