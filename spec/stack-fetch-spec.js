a5f1bd8db2cea808bbfbaf64b603e88d469a0cc9
izzydoesit <israeldmatos@gmail.com>
2018-03-02 11:08:22 -0800

    Initial commit - generate package

---
+++ spec/stack-fetch-spec.js
@@ -0,0 +1,73 @@
+'use babel';
+
+import StackFetch from '../lib/stack-fetch';
+
+// Use the command `window:run-package-specs` (cmd-alt-ctrl-p) to run specs.
+//
+// To run a specific `it` or `describe` block add an `f` to the front (e.g. `fit`
+// or `fdescribe`). Remove the `f` to unfocus the block.
+
+describe('StackFetch', () => {
+  let workspaceElement, activationPromise;
+
+  beforeEach(() => {
+    workspaceElement = atom.views.getView(atom.workspace);
+    activationPromise = atom.packages.activatePackage('stack-fetch');
+  });
+
+  describe('when the stack-fetch:toggle event is triggered', () => {
+    it('hides and shows the modal panel', () => {
+      // Before the activation event the view is not on the DOM, and no panel
+      // has been created
+      expect(workspaceElement.querySelector('.stack-fetch')).not.toExist();
+
+      // This is an activation event, triggering it will cause the package to be
+      // activated.
+      atom.commands.dispatch(workspaceElement, 'stack-fetch:toggle');
+
+      waitsForPromise(() => {
+        return activationPromise;
+      });
+
+      runs(() => {
+        expect(workspaceElement.querySelector('.stack-fetch')).toExist();
+
+        let stackFetchElement = workspaceElement.querySelector('.stack-fetch');
+        expect(stackFetchElement).toExist();
+
+        let stackFetchPanel = atom.workspace.panelForItem(stackFetchElement);
+        expect(stackFetchPanel.isVisible()).toBe(true);
+        atom.commands.dispatch(workspaceElement, 'stack-fetch:toggle');
+        expect(stackFetchPanel.isVisible()).toBe(false);
+      });
+    });
+
+    it('hides and shows the view', () => {
+      // This test shows you an integration test testing at the view level.
+
+      // Attaching the workspaceElement to the DOM is required to allow the
+      // `toBeVisible()` matchers to work. Anything testing visibility or focus
+      // requires that the workspaceElement is on the DOM. Tests that attach the
+      // workspaceElement to the DOM are generally slower than those off DOM.
+      jasmine.attachToDOM(workspaceElement);
+
+      expect(workspaceElement.querySelector('.stack-fetch')).not.toExist();
+
+      // This is an activation event, triggering it causes the package to be
+      // activated.
+      atom.commands.dispatch(workspaceElement, 'stack-fetch:toggle');
+
+      waitsForPromise(() => {
+        return activationPromise;
+      });
+
+      runs(() => {
+        // Now we can test for view visibility
+        let stackFetchElement = workspaceElement.querySelector('.stack-fetch');
+        expect(stackFetchElement).toBeVisible();
+        atom.commands.dispatch(workspaceElement, 'stack-fetch:toggle');
+        expect(stackFetchElement).not.toBeVisible();
+      });
+    });
+  });
+});
